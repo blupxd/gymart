6e258929042cd552044d47be6bc4f8df4bac5282
blupxd <matija.stefanovic050@gmail.com>
2024-08-12 14:05:43 +0200

    fixovano

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="sr">
       <body className={mont.className}>
       <Navbar />
       {children}
